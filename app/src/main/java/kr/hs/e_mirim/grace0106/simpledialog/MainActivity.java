6d32a19183c05776941bdd57b0bf7da6bd217a16
HyeonJu Jeong <0106grace@naver.com>
2016-06-07 20:27:45 +0900

    #3 Dialog.setPositiveButton

--- app/src/main/java/kr/hs/e_mirim/grace0106/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/e_mirim/grace0106/simpledialog/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -23,7 +24,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         dialog.setTitle("First Dialog");
         dialog.setMessage("This is message part.");
         dialog.setIcon(R.drawable.first_icon);
-        dialog.setPositiveButton("확인", null);
+        dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this,"대화상자의 확인 버튼을 클릭했음.",Toast.LENGTH_LONG).show();
+
+            }
+        });
         dialog.show();
     }
 }
